7d712409fc18ae2993a3ba9ddc714af2005cfe2d
Will Thompson <will.k.t@gmail.com>
2021-02-16 14:47:51 -0600

    removing old week 6 directory

--- 6_resilient_code/examples/increment.R
+++
@@ -1,9 +0,0 @@
-########
-## Unit Tests in R
-########
-
-# example taken from https://towardsdatascience.com/unit-testing-in-r-68ab9cc8d211
-
-increment <- function(value) {
-  value + 1
-}
--- 6_resilient_code/examples/script_ex.py
+++
@@ -1,8 +0,0 @@
-##################################
-# Run script till it gets an error
-##################################
-
-import time
-
-time.sleep(2)
-raise Exception("Oh no, this script just died")
