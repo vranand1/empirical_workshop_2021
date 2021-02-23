7fae942fa3d8a3fe98a7a405bd539a5314c21740
Will Thompson <will.k.t@gmail.com>
2021-02-23 12:55:21 -0600

    adding 7th workshop lecture

---
+++ 7_accelerate/notebooks/ipython_cell_input.py
@@ -0,0 +1,23 @@
+
+import random
+
+# Example from: https://towardsdatascience.com/speed-up-jupyter-notebooks-20716cbe2025
+def estimate_pi(n=1e6) -> "area":
+    """Estimate pi with monte carlo simulation.
+    
+    Arguments:
+        n: number of simulations
+    """
+    in_circle = 0
+    total = n
+    
+    while n != 0:
+        prec_x = random.random()
+        prec_y = random.random()
+        if pow(prec_x, 2) + pow(prec_y, 2) <= 1:
+            in_circle += 1 # inside the circle
+        n -= 1
+        
+    return 4 * in_circle / total
+
+estimate_pi(n=1e3)
